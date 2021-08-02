e853271423c431df5ff9ab5470d109f86a27dea9
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-08-02 10:49:19 -0300

    agregando estructura

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app = express ();
+const path = require ('path');
+const port = 3030;
+ 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto' + port));
