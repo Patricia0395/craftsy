c0cec531530a9e4dbacc01695b04c3df3fdf5c1b
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-08-24 09:10:47 -0300

    nose

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.htm
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
+
 app.use(express.static('public'));
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto' + port));
